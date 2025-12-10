453bf692e57f91ecec07252f98d6e9e616f7b997
minhajzein <minhajzein@gmail.com>
2025-12-11 00:13:56 +0530

    footer and 3d model updated

--- src/components/footer/ExploreSection.tsx
+++ src/components/footer/ExploreSection.tsx
@@ -18,11 +18,11 @@ function ExploreSection() {
 					Start Your Real Estate Journey Today
 				</h1>
 				<p className='text-gray-400 text-[16px]'>
-					Your dream property is just a click away. Whether you're looking for a
+					{`Your dream property is just a click away. Whether you're looking for a
 					new home, a strategic investment, or expert real estate advice,
 					Estatein is here to assist you every step of the way. Take the first
 					step towards your real estate goals and explore our available
-					properties or get in touch with our team for personalized assistance.
+					properties or get in touch with our team for personalized assistance.`}
 				</p>
 			</div>
 			<Button variant='default' className='z-40'>
